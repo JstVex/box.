b8ccd7261e80a02c0ac0f0051fa0c6924e38ebd3
JstVex <heinhtetlulu14@gmail.com>
2023-11-27 21:06:56 -0800

    Create half screen b/w with two boxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      box
+      {/* <div>
+        box.
+      </div> */}
+      <div className="flex w-full h-screen">
+        <div className="relative flex flex-col items-center justify-center w-[50vw] h-screen bg-black text-white">
+          <div className="absolute right-4 top-28 text-6xl font-semibold">
+            Choose
+          </div>
+          <Link href="/whitebox" className="w-40 h-40 bg-white transform transition-transform duration-300 origin-center hover:rotate-45 cursor-pointer"></Link>
+        </div>
+        <div className="relative flex flex-col items-center justify-center w-[50vw] h-screen bg-white text-black">
+          <div className="absolute left-4 top-28 text-6xl font-semibold">
+            a box
+          </div>
+          <Link href="/blackbox" className="w-40 h-40 bg-black transform transition-transform duration-300 origin-center hover:rotate-45 cursor-pointer"></Link>
+        </div>
+      </div>
     </div>
   )
 }
